a0bee6111b8a7fdaab1437029483e7885be5b005
NestorSanchez <nesaenz.69@gmail.com>
2023-05-20 13:41:30 -0500

    Modal set successfully

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Container, Typography, Modal } from '@material-ui/core';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Container, Typography, Modal, makeStyles, Backdrop, Fade } from '@material-ui/core';
 
 interface Film {
   title: string;
@@ -15,16 +15,31 @@ async function fetchFilm(url: string): Promise<Film> {
   return data;
 }
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Films() {
   const { search } = useLocation();
   const queryParams = new URLSearchParams(search);
   const films = JSON.parse(queryParams.get('films') || '[]');
 
-  const [filmIndex, setFilmIndex] = useState(0);
-  const [film, setFilm] = useState<Film | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(true); // Variable de estado para controlar la apertura y el cierre del modal
+  const [filmIndex, setFilmIndex] = React.useState(0);
+  const [film, setFilm] = React.useState<Film | null>(null);
+  const [open, setOpen] = React.useState(true); // Estado para controlar la apertura y cierre del modal
 
-  useEffect(() => {
+  const navigate = useNavigate(); // Historial de navegación de React Router
+
+  React.useEffect(() => {
     const fetchAndSetFilm = async () => {
       if (filmIndex >= 0 && filmIndex < films.length) {
         const fetchedFilm = await fetchFilm(films[filmIndex]);
@@ -44,32 +59,45 @@ function Films() {
   };
 
   const handleClose = () => {
-    setIsModalOpen(false); // Actualiza la variable de estado para cerrar el modal
-    setFilm(null);
+    setOpen(false); // Cerrar el modal estableciendo el estado de "open" en false
+    navigate(-1); // Regresar al componente anterior utilizando la función goBack() del historial de navegación
   };
+
+  const classes = useStyles();
 
   if (!film) {
     return null;
   }
 
   return (
-    <Modal open={isModalOpen} onClose={handleClose}>
-      <div>
-        <Container>
-          <Typography variant="h4" component="h2">
-            {film.title}
-          </Typography>
-          <Typography color="textSecondary">Director: {film.director}</Typography>
-          <Typography color="textSecondary">Release Date: {film.release_date}</Typography>
-          <Typography>{film.opening_crawl}</Typography>
-          {filmIndex > 0 && (
-            <button onClick={handlePreviousFilm}>Previous Film</button>
-          )}
-          {filmIndex < films.length - 1 && (
-            <button onClick={handleNextFilm}>Next Film</button>
-          )}
-        </Container>
-      </div>
+    <Modal
+      open={open} // Utilizar el estado "open" para controlar la apertura y cierre del modal
+      onClose={handleClose}
+      className={classes.modal}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <div className={classes.paper}>
+          <Container>
+            <Typography variant="h4" component="h2">
+              {film.title}
+            </Typography>
+            <Typography color="textSecondary">Director: {film.director}</Typography>
+            <Typography color="textSecondary">Release Date: {film.release_date}</Typography>
+            <Typography>{film.opening_crawl}</Typography>
+            {filmIndex > 0 && (
+              <button onClick={handlePreviousFilm}>Previous Film</button>
+            )}
+            {filmIndex < films.length - 1 && (
+              <button onClick={handleNextFilm}>Next Film</button>
+            )}
+          </Container>
+        </div>
+      </Fade>
     </Modal>
   );
 }
